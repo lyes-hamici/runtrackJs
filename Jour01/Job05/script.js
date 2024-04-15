a81f1e506f2fb59fcc12b0f551a69e925a175e7e
lyes-hamici <lyes.hamici@laplateforme.io>
2024-04-15 09:50:19 +0200

    <feat>[Job01 to Job05]:<creation of the function>

---
+++ Jour01/Job05/script.js
@@ -0,0 +1,9 @@
+function afficherJoursSemaines(){
+    const jour = []
+    jour.push("lundi", "mardi", "mercedi","jeudi","vendredi","samedi","dimanche")
+    for (let i = 0; i < jour.length; i++) {
+        console.log(jour[i]);
+      } 
+    }
+
+afficherJoursSemaines()
